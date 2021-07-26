8806b8bab27089938e25e78f9e2626e03bfa9405
pwplains9 <pwplains911@gmail.com>
2021-07-26 15:36:43 +0300

    Update helpers.js

--- routers/helpers.js
+++ routers/helpers.js
@@ -1,15 +1,6 @@
 const $html = $('html');
 
 export default {
-
-	isMobile() {
-		return innerWidth <= 1024;
-	},
-
-	isIEorEdge() {
-		return $html.hasClass('is-browser-ie') || $html.hasClass('is-browser-edge');
-	},
-
 	setPage(name) {
 		if (!name) {
 			return;
