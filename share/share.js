569bbbe19f011347f6b25c46d42e7d6d95cac6e3
pwplains9 <pwplains911@gmail.com>
2021-08-02 09:29:14 +0300

    Delete share directory

--- share/share.js
+++
@@ -1,8 +0,0 @@
-import Share from 'ninelines-sharing';
-
-$('[data-social]').on('click', (e) => {
-	let social = $(e.currentTarget).data('social');
-	let url = location.origin + location.pathname + location.search;
-
-	Share[social](url);
-});
