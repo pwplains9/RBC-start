5b94053186abdffe4cef867389fdda2806aa5b62
pwplains9 <pwplains911@gmail.com>
2021-12-20 17:33:09 +0300

    Update share.js

--- share/share.js
+++ share/share.js
@@ -1,8 +1,45 @@
 import Share from 'ninelines-sharing';
+import vars from "../helpers";
 
-$('[data-social]').on('click', (e) => {
-	let social = $(e.currentTarget).data('social');
-	let url = location.origin + location.pathname + location.search;
+const init = () => {
+	if (document.querySelector('[data-social]')) {
+		const list = document.querySelectorAll('[data-social]');
 
-	Share[social](url);
-});
+		Array.prototype.forEach.call(list, (item) => {
+			item.addEventListener('click', (e) => {
+				let shareWindow = Share.openWindow('');
+				const social = e.currentTarget.dataset.social;
+
+				let options = $.extend({
+					url: location.href,
+					title: document.title,
+					image: vars.$document.find('meta[property="og:image"]').attr('content'), // Заполняем шеринг на странице .html
+					text: document.querySelector('meta[name="description"]').content,
+				});
+
+				let urlImage = location.origin + options.image;
+
+				let url = location.origin + location.pathname;
+
+				let urlVK = `${'http://vkontakte.ru/share.php?'
+				+ 'url='}${encodeURIComponent(options.url)}
+					&title=${encodeURIComponent(options.title)}
+					&description=${encodeURIComponent(options.text)}
+					&image=${encodeURIComponent(urlImage)}
+					&noparse=true`;
+
+				shareWindow.location = {
+					facebook: `https://facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`,
+					vk: urlVK,
+					twitter: `http://twitter.com/share?url=${encodeURIComponent(url)}`,
+					ok: `https://connect.ok.ru/offer?url=${encodeURIComponent(url)}`,
+				}[social];
+			});
+		});
+	}
+};
+
+export default {
+	init,
+};
+
